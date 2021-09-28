e668224d2900725aa648f43a4e7b619b3dcb304e
jiahaodavid <jiahaodavid4040@gmail.com>
2021-09-28 11:19:49 -0700

    init

--- MainCode/MainApp.py
+++ MainCode/MainApp.py
@@ -2,40 +2,45 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-#visualizations from other files
+# visualizations from other files
 from ResearchQ1 import graph_1_1, graph_1_2, graph_1_3, graph_1_4
 from ResearchQ2 import graph_2_1
 from ResearchQ3 import graph_3_1
-#other dependencies: statsmodels
+# other dependencies: statsmodels
 
-#creating the dash app
+# creating the dash app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#initial figure value
+# initial figure value
 fig = graph_1_1()
 
-#updating layout with the figure
+# updating layout with the figure
 app.layout = html.Div([
     dcc.Dropdown(
         id='chosenViz',
         options=[
-            #add your graphs to the dropdown menu here, value rq1g1 stands for
-            #research question 1 graph 1, etc, add elif statement in the callback
-            #after adding a graph
-            {'label': 'NYC Air Quality vs Prosperity (2017)', 'value': 'rq1g1'},
+            # add your graphs to the dropdown menu here, value rq1g1 stands for
+            # research question 1 graph 1, etc, add elif statement in the callback
+            # after adding a graph
+            {'label': '2019 Avg PM2.5 Concentration in NYC', 'value': 'rq3g1'},
+            {'label': 'NYC Air Quality vs Prosperity (2017)',
+             'value': 'rq1g1'},
             {'label': 'NYC Air Quality vs Income (2017)', 'value': 'rq1g2'},
-            {'label': 'NYC Air Quality vs Population (2017)', 'value': 'rq1g3'},
+            {'label': 'NYC Air Quality vs Population (2017)',
+             'value': 'rq1g3'},
             {'label': 'NYC Air Quality vs Borough (2017)', 'value': 'rq1g4'},
-            {'label': 'NYC 2019 Air Quality', 'value': 'rq2g1'},
-            {'label': '2019 Avg PM2.5 Concentration in NYC', 'value': 'rq3g1'}
+
+
         ],
-        value='rq1g1'
+        value='rq3g1'
     ),
     dcc.Graph(id='graph-with-slider', figure=fig, style={'height': '90vh'})
 ])
 
-#handling a change in dropdown selection
+# handling a change in dropdown selection
+
+
 @app.callback(
     dash.dependencies.Output('graph-with-slider', 'figure'),
     [dash.dependencies.Input('chosenViz', 'value')])
@@ -52,6 +57,7 @@ def update_output(value):
         return graph_2_1()
     elif value == "rq3g1":
         return graph_3_1()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
